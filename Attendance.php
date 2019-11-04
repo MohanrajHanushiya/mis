ed75666c721e38a0615f9c22d0b00d545347f352
MohanrajHanushiya <54566917+MohanrajHanushiya@users.noreply.github.com>
2019-11-04 09:16:26 +0530

    Update Attendance.php

--- Attendance.php
+++ Attendance.php
@@ -10,6 +10,10 @@ include_once ("attendancenav.php");
 <!-- end dont change the order-->
 
 
+<!-- search coding -->
+
+
+
 <!-- Block#2 start your code -->
 
 <div class="container" style="margin-top:30px">
@@ -25,9 +29,9 @@ include_once ("attendancenav.php");
 <div class="row">
 <div class="col-sm-9 " ></div>
 <div class="col-sm-3 " > 
-<form class="form-inline md-form form-sm mt-4">
-<input class="form-control form-control-sm ml-3 w-75 rounded-pill" type="text" placeholder="Search ID" aria-label="Search"id="search"> 
-<i class="fas fa-search ml-3" aria-hidden="true"></i>
+<form class="example" action=".php" style="margin:auto;max-width:300px">
+  <input type="text" placeholder="Search.." name="search2">
+  <button type="submit"><i class="fa fa-search"></i></button>
 </form>
 </div>
 </div>
@@ -36,31 +40,79 @@ include_once ("attendancenav.php");
 <div class="table-responsive">
 <table class="table table-striped table-bordered" id="student_table"> <thead>
 <tr>
-<th>Student Name</th>
+<th>A ID</th>
 <th>Roll Number</th>
 <th>Module</th>
 <th>Module head</th>
 <th>Attendance Percentage</th>
-<th>Report</th>
+<th>option</td>
+<!-- <th>Report</th>  -->
 </tr>
-<tr>
-<td>fjhfj</td>
-<td>fjhfj</td>
-<td>fjhfj</td>
-<td>fjhfj</td>
-<td>70%</td>
-<td> <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Report</button>
-</td>
-</tr>
-</thead>
-<tbody>
+
+<?php
+
+if(isset($_GET['delete']))
+{
+    $attendance_id = $_GET['delete'];
+    // echo 'Your sql code is here'.$result_id;
+    $sql = "Delete From `attendance` where attendance_id = $attendance_id ";
+    if(mysqli_query($con,$sql)){
+        echo"Record has been deleted succesfully";
+    }
+    else {
+        echo "Error deleting" . mysqli_error($con);
+    }
+}
+?>
+
+<?php
+
+//for insert
+
+$sql = "SELECT attendance_id,student_id,module_name,staff_name,attendance_status FROM attendance";
+$result = mysqli_query($con,$sql);
+if(mysqli_num_rows($result)>0)
+{ //output data of each row
+    while($row = mysqli_fetch_assoc($result))
+    {
+        echo '
+        <tr>
+        <td>'. $row["attendance_id"] . "<br>" .'</td>
+        <td>'. $row["student_id"] . "<br>" .'</td>
+        <td>'. $row["module_name"]."<br>".'</td>
+          <td>'. $row["staff_name"]."<br>".'</td>
+          <td>'. $row["attendance_status"]."<br>".'</td>
+          <td>   
+                   
+                    <button class="btn btn-sm btn-danger" data-href="?delete='.$row["attendance_id"].'" data-toggle="modal" data-target="#confirm-delete"><i class="fas fa-trash"></i> </button>     
+            </td>  
+   
+        </tr>';
+    }
+}
+else {
+        echo "0 results";
+    }
+?>
+  
+
+
+
+
+
 </tbody>
 </table>
+ 
 
 <nav aria-label="Page navigation example">
+
 <ul class="pagination justify-content-end">
+
+
+
 <li class="page-item ">
-<a class="page-link" href="#" tabindex="-1">Previous</a>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Report</button>
+<!-- <a class="page-link" href="#" tabindex="-1">Previous</a>
 </li>
 <li class="page-item"><a class="page-link" href="#">1</a></li>
 <li class="page-item"><a class="page-link" href="#">2</a></li>
@@ -69,7 +121,8 @@ include_once ("attendancenav.php");
 <a class="page-link" href="#">Next</a>
 </li>
 </ul>
-</nav>
+</nav> -->
+
 </div>
 </div>
 </div>
